027f38d1c71a0086ee75867b26fa0c862b93de2b
zainamroti <sayedzainjeelani@gmail.com>
2022-05-11 14:12:26 +0500

    nft mint done

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -2,11 +2,17 @@
 
 const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
-  const myNftContract = await nftContractFactory.deploy("https://epic-nfts-bs-2.vercel.app/api/");
+  const myNftContract = await nftContractFactory.deploy("https://epic-nfts-bs-2.vercel.app/api/0");
 
-  await myNftContract.deployed(); 
+  await myNftContract.deployed();
 
   console.log(`NFT Deployed Address: ${myNftContract.address}`);
+
+  // Call the function.
+  let txn = await myNftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+  console.log("Minted NFT #1")
 }
 
 main().then(() => process.exit(0)).catch(() => {
