53781ba118c9230bf81c391d14d9ee989b7fd177
zainamroti <sayedzainjeelani@gmail.com>
2022-05-14 00:03:11 +0500

    added verify method and contract verified.

--- smart-contracts/scripts/run.js
+++ smart-contracts/scripts/run.js
@@ -1,29 +1,35 @@
 
 
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
-    const myNftContract = await nftContractFactory.deploy();
+    // const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+    // const myNftContract = await nftContractFactory.deploy();
 
-    await myNftContract.deployed();
+    // await myNftContract.deployed();
 
-    console.log(`NFT Deployed Address: ${myNftContract.address}`);
+    // console.log(`NFT Deployed Address: ${myNftContract.address}`);
 
-    myNftContract.on("NewEpicNFTMinted", (from, tokenId) => {
-        console.log(from, tokenId.toNumber())
-        alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+    // myNftContract.on("NewEpicNFTMinted", (from, tokenId) => {
+    //     console.log(from, tokenId.toNumber())
+    //     alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+    // });
+
+    // // Call the function.
+    // let txn = await myNftContract.makeAnEpicNFT()
+    // // Wait for it to be mined.
+    // await txn.wait()
+    // console.log("Minted NFT #1");
+
+    // // Call the function.
+    // txn = await myNftContract.mintIpfsNFT("")
+    // // Wait for it to be mined.
+    // await txn.wait()
+    // console.log("Minted NFT #2");
+
+    await hre.run("verify:verify", {
+        address: "0x0862F81814897171C4b879A912c0C167B67Df79F",
+        constructorArguments: [],
     });
 
-    // Call the function.
-    let txn = await myNftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-    console.log("Minted NFT #1");
-
-    // Call the function.
-    txn = await myNftContract.mintIpfsNFT("")
-    // Wait for it to be mined.
-    await txn.wait()
-    console.log("Minted NFT #2");
 }
 
 main().then(() => process.exit(0)).catch((error) => {
